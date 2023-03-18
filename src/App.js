d47efbeefdaea98338a2f04f3b6d94dcdc1443ea
SpoonMouse <tracyd153@gmail.com>
2023-03-17 20:15:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Cardlist from './components/Cardlist/cardlist'
+import SearchBox from './components/SearchBox/searchbox';
+import Scroll from './components/Scroll/scroll'
+import ErrorBoundry from './components/ErrorBoundry/error';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+      .then(users => {
+          this.setState({ robots: users })})};
+
+    onSearchChange = (e) => {
+      this.setState({searchfield: e.target.value})
+  }
+
+  render() {
+    const { robots, searchfield } = this.state ;
+    const filteredRobots=robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+     return !robots.length ? 
+        <h1>Loading</h1> :
+       (
+    <div className="App tc">
+      <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+              <Cardlist robots={filteredRobots}/>
+            </ErrorBoundry>
+          </Scroll>
     </div>
-  );
+      );
 }
+}
+
+
 
 export default App;
